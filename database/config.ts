9477440010c157660ec639c77a7939de3b26fccb
Milagros Marquina Mora <64057269+milagros-marquina-jumi@users.noreply.github.com>
2024-04-26 23:27:45 -0500

    base project

---
+++ database/config.ts
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const dbConnection = async () => {
+
+
+    try {
+    //    await mongoose.connect(process.env.MOGODB_CNN);
+       console.log('se conecto la base de datos');
+     }
+    catch (error) {
+        console.error(error);
+        throw new Error('error al iniciar la base de datos ')
+    }
+}
+
+module.exports= {
+    dbConnection
+}
