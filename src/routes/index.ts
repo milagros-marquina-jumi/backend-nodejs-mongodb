44a5d5d44be5bb3bbebe40595bafa37fe9d87ace
Milagros Marquina Mora <64057269+milagros-marquina-jumi@users.noreply.github.com>
2024-05-01 17:50:54 -0500

    Health Check to verify API status

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,6 +5,10 @@ import usersRouter from './user'
 
  function routerApi(app:Express) {
   const router =  Router();
+
+  app.get('/api/health', (req, res) => {
+    res.json({ success: true })
+  });
   app.use('/api/v1', router);
   router.use('/auth', authRouter);
   router.use('/user', usersRouter);
